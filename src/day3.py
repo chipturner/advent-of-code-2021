f4537a4ede23bc6863c5a3dff89f1a158cd02e11
Chip Turner <cturner@pattern.net>
2021-12-02 21:25:10 -0800

    gross pt2

--- src/day3.py
+++ src/day3.py
@@ -4,23 +4,45 @@ import itertools
 import collections
 
 def main() -> None:
-    lines = helpers.read_input()
+    orig_lines = helpers.read_input()
+    lines = orig_lines
     v1, v2 = '', ''
+    oxy_sel = ''
     for i in range(len(lines[0])):
+        print(lines)
         ones, zeroes = 0, 0
         for line in lines:
             if line[i] == '0':
                 zeroes += 1
             else:
                 ones += 1
-        if ones > zeroes:
-            v1 += '1'
-            v2 += '0'
-        else:
-            v1 += '0'
-            v2 += '1'
-    print(int(v1, 2))
-    print(int(v2, 2))
-    print(int(v2, 2) * int(v1, 2))
+        mcb = str(int(ones >= zeroes))
+        print(mcb)
+        lines = [ l for l in lines if l[i] == mcb ]
+        oxy_sel = lines[0]
+        if len(lines) == 1:
+            break
+    print(oxy_sel)
+
+    lines = orig_lines
+    v1, v2 = '', ''
+    co2_sel = ''
+    for i in range(len(lines[0])):
+        print(lines)
+        ones, zeroes = 0, 0
+        for line in lines:
+            if line[i] == '0':
+                zeroes += 1
+            else:
+                ones += 1
+        mcb = str(int(ones < zeroes))
+        print(mcb)
+        lines = [ l for l in lines if l[i] == mcb ]
+        co2_sel = lines[0]
+        if len(lines) == 1:
+            break
+    print(co2_sel)
+
+    print(int(co2_sel, 2) * int(oxy_sel, 2))
 
 main()
