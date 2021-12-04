533d7c6a7ce297cc47bd4f849ea8d7cc2d666450
Chip Turner <cturner@pattern.net>
2021-12-03 20:32:45 -0800

    play with day3 in numpy

--- src/day3.py
+++ src/day3.py
@@ -1,48 +1,34 @@
+import numpy
 import helpers
 
 import itertools
 import collections
 
+def mcb(r):
+    return numpy.sum(r == 1) >= numpy.sum(r == 0)
+
 def main() -> None:
-    orig_lines = helpers.read_input()
-    lines = orig_lines
+    orig_data = helpers.read_input_matrix()
+    data = orig_data.copy()
     v1, v2 = '', ''
     oxy_sel = ''
-    for i in range(len(lines[0])):
-        print(lines)
-        ones, zeroes = 0, 0
-        for line in lines:
-            if line[i] == '0':
-                zeroes += 1
-            else:
-                ones += 1
-        mcb = str(int(ones >= zeroes))
-        print(mcb)
-        lines = [ l for l in lines if l[i] == mcb ]
-        oxy_sel = lines[0]
-        if len(lines) == 1:
+    for i in range(len(data[0])):
+        transposed = data.transpose()
+        m = mcb(transposed[i])
+        data = numpy.array([r for r in data if r[i] == m])
+        if len(data) == 1:
             break
-    print(oxy_sel)
 
-    lines = orig_lines
-    v1, v2 = '', ''
-    co2_sel = ''
-    for i in range(len(lines[0])):
-        print(lines)
-        ones, zeroes = 0, 0
-        for line in lines:
-            if line[i] == '0':
-                zeroes += 1
-            else:
-                ones += 1
-        mcb = str(int(ones < zeroes))
-        print(mcb)
-        lines = [ l for l in lines if l[i] == mcb ]
-        co2_sel = lines[0]
-        if len(lines) == 1:
+    v1 = int(''.join(map(str, data.flatten().tolist())), 2)
+
+    data = orig_data.copy()
+    for i in range(len(data[0])):
+        transposed = data.transpose()
+        m = mcb(transposed[i]) == 0
+        data = numpy.array([r for r in data if r[i] == m])
+        if len(data) == 1:
             break
-    print(co2_sel)
-
-    print(int(co2_sel, 2) * int(oxy_sel, 2))
+    v2 = int(''.join(map(str, data.flatten().tolist())), 2)
+    print(v1 * v2)
 
 main()
