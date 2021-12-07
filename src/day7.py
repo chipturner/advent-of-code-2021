a2620143efa6cd83131eb4c6476256e568bc16e2
Chip Turner <cturner@pattern.net>
2021-12-06 21:11:18 -0800

    pt2

--- src/day7.py
+++ src/day7.py
@@ -4,12 +4,15 @@ import math
 import itertools
 import collections
 
+def cost(d):
+    d = abs(d)
+    return d * (d + 1) // 2
 
 def main() -> None:
     input = helpers.read_input_numbers()
     d = sorted(input)
-    for i in d:
-        print(i, sum(abs(i - n) for n in d))
+    for i in range(min(d), max(d) + 1):
+        print(i, sum(cost(i - n) for n in d))
 
 
 main()
