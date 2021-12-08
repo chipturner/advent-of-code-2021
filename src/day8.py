94bdd9f7a7545856237060aadb0f965f14b08be3
Chip Turner <cturner@pattern.net>
2021-12-07 23:22:30 -0800

    another speedup because why not

--- src/day8.py
+++ src/day8.py
@@ -15,9 +15,8 @@ def main() -> None:
     lines = [ l.split(' | ') for l in helpers.read_input()]
     mappings = [ str.maketrans(dict(zip(permutation, segments))) for permutation in itertools.permutations(segments) ]
     for left, right in lines:
+        entries = [ ''.join(sorted(w)) for w in left.split(' ') + right.split(' ')]
         for mapping in mappings:
-            entries = [ ''.join(sorted(w)) for w in left.split(' ') + right.split(' ')]
-
             xformed_valid_numbers = { xform(k, mapping): v for k, v in valid_numbers.items() }
             if all(entry in xformed_valid_numbers for entry in entries):
                 new_left = ''.join([xformed_valid_numbers[''.join(sorted(v))] for v in left.split()])
