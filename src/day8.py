1683148dee93a954709ae10c071351574ba59aab
Chip Turner <cturner@pattern.net>
2021-12-07 23:15:07 -0800

    some optimizations

--- src/day8.py
+++ src/day8.py
@@ -13,16 +13,16 @@ def xform(s: str, m: Dict[int, str]) -> str:
 
 def main() -> None:
     lines = [ l.split(' | ') for l in helpers.read_input()]
+    mappings = [ str.maketrans(dict(zip(permutation, segments))) for permutation in itertools.permutations(segments) ]
     for left, right in lines:
-        okay_perm = None
-        for permutation in itertools.permutations(segments):
-            mapping = str.maketrans(dict(zip(permutation, segments)))
+        for mapping in mappings:
             entries = [ ''.join(sorted(w)) for w in left.split(' ') + right.split(' ')]
-            xformed_entries = [ xform(e, mapping) for e in entries]
+
             xformed_valid_numbers = { xform(k, mapping): v for k, v in valid_numbers.items() }
             if all(entry in xformed_valid_numbers for entry in entries):
                 new_left = ''.join([xformed_valid_numbers[''.join(sorted(v))] for v in left.split()])
                 new_right = ''.join([xformed_valid_numbers[''.join(sorted(v))] for v in right.split()])
                 print(f'yay {new_left} | {new_right}')
+                break
 
 main()
