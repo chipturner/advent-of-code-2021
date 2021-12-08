3316e8044565e641df65f6681594b97804f78606
Chip Turner <cturner@pattern.net>
2021-12-07 23:05:09 -0800

    use maketrans

--- src/day8.py
+++ src/day8.py
@@ -8,15 +8,15 @@ import collections
 valid_numbers = dict(abcefg='0', cf='1', acdeg='2', acdfg='3', bcdf='4', abdfg='5', abdefg='6', acf='7', abcdefg='8', abcdfg='9')
 segments = 'abcdefg'
 
-def xform(s: str, m: Dict[str, str]) -> str:
-    return ''.join(sorted(m[c] for c in s))
+def xform(s: str, m: Dict[int, str]) -> str:
+    return ''.join(sorted(s.translate(m)))
 
 def main() -> None:
     lines = [ l.split(' | ') for l in helpers.read_input()]
     for left, right in lines:
         okay_perm = None
         for permutation in itertools.permutations(segments):
-            mapping = dict(zip(permutation, segments))
+            mapping = str.maketrans(dict(zip(permutation, segments)))
             entries = [ ''.join(sorted(w)) for w in left.split(' ') + right.split(' ')]
             xformed_entries = [ xform(e, mapping) for e in entries]
             xformed_valid_numbers = { xform(k, mapping): v for k, v in valid_numbers.items() }
