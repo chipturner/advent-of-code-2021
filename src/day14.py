86e1e0c659faf0b316a155cdd58d44189a066df6
Chip Turner <cturner@pattern.net>
2021-12-13 22:35:52 -0800

    pt2

--- src/day14.py
+++ src/day14.py
@@ -12,24 +12,30 @@ def main() -> None:
         k, v = r.split(' -> ')
         rules[k] = v
     
-
+    pairs = collections.defaultdict(int)
     poly = lines[0]
-    step = 1
-    for r in rules:
-        i = 0
-        while i < len(poly):
-            m = rules.get(poly[i:i+2], None)
-            if m:
-                poly = poly[:i+1] + m + poly[i+1:]
-                i += 1
-            i += 1
-        print('step', step, poly)
-        step += 1
-        if step == 11:
-            break
-    counts = collections.defaultdict(int)
-    for c in poly:
-        counts[c] += 1
-    print(max(counts.values()) - min(counts.values()))
+    for a, b in zip(poly[:-1], poly[1:]):
+        pairs[a+b] += 1
+
+    for step in range(1, 1000):
+        print('pairs', pairs)
+        new_pairs = collections.defaultdict(int)
+        for p, count in pairs.items():
+            if p not in rules:
+                new_pairs[p] = count
+            else:
+                new_pairs[p[0] + rules[p]] += count
+                new_pairs[rules[p] + p[1]] += count
+        pairs = new_pairs
+        print('new_pairs', pairs)
+        
+        counts = collections.defaultdict(int)
+        counts[poly[0]] += 1
+        counts[poly[-1]] += 1
+        for k, c in pairs.items():
+            counts[k[0]] += c
+            counts[k[1]] += c
+        print('step', step, 'counts', counts)
+        print(max(counts.values())//2 - min(counts.values())//2)
 
 main()
