b84b26b6268ab5a38961ca414a644f1b4fb8f632
Chip Turner <cturner@pattern.net>
2021-12-15 23:09:37 -0800

    simplify with stringio

--- src/day16.py
+++ src/day16.py
@@ -4,6 +4,7 @@ import itertools
 import collections
 import operator
 import functools
+import io
 
 def prod(args):
     return functools.reduce(operator.mul, args)
@@ -19,62 +20,52 @@ opmap = [ sum, prod, min, max, None, gt, lt, eq]
 # packet: 3 bit ver, 3 bit type
 # type 4 == literal (5 bits,last starts with 0)
 def decode_packet(p):
-    ver, type_id = int(p[0:3], 2), int(p[3:6], 2)
-    off = 6
+    ver, type_id = int(p.read(3), 2), int(p.read(3), 2)
     print('decode ver', ver, 'type', type_id, p)
 
     sub_value = None
     if type_id == 4:
         s = ''
-        while p[off] != '0':
-            nib = p[off+1:off+5]
+        while p.read(1) != '0':
+            nib = p.read(4)
             s += nib
-            off += 5
-        s += p[off+1:off+5]
+        s += p.read(4)
         literal = int(s, 2)
         print('literal', s, literal)
-        off += 5
         sub_value = literal
     else:
-        length_type_id = p[off]
-        off += 1
+        length_type_id = p.read(1)
         print(type_id)
         print('op is', opmap[type_id])
         if length_type_id == '0':
-            nib = p[off:off+15]
+            nib = p.read(15)
             total_length = int(nib, 2)
-            off += 15
-            end = off + total_length
             print('subpacket length', total_length)
+            p = io.StringIO(p.read(total_length))
             acc = []
-            while off < end:
-                consumed, val = decode_packet(p[off:end])
-                off += consumed
+            while p.tell() < total_length:
+                val = decode_packet(p)
                 acc.append(val)
             print(opmap[type_id], acc)
             sub_value = opmap[type_id](acc)
         else:
-            nib = p[off:off+11]
+            nib = p.read(11)
             num_subs = int(nib, 2)
             print('subpacket group', num_subs)
-            off += 11
             acc = []
             for i in range(num_subs):
-                consumed, val = decode_packet(p[off:])
-                off += consumed
+                val = decode_packet(p)
                 acc.append(val)
             print(opmap[type_id], acc)
             sub_value = opmap[type_id](acc)
-    return off, sub_value
-
-def xlate(s):
-    return ''.join(f'{int(c, 16):04b}' for c in s)
+    return sub_value
 
 def main() -> None:
     lines = helpers.read_input()
     if 'A' in lines[0]:
-        print(decode_packet(xlate(lines[0])))
+        s = bin(int(lines[0], 16))[2:]
     else:
-        print(decode_packet(lines[0]))
+        s = lines[0]
+    print(decode_packet(io.StringIO(s)))
 
 main()
