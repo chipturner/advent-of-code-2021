09bd26d5899ebcf4c4090a32b7e99dfa33e252d4
Chip Turner <cturner@pattern.net>
2021-12-20 10:11:07 -0800

    nice refactor

--- src/day20.py
+++ src/day20.py
@@ -11,6 +11,15 @@ def zero_trim_2d(img):
     p = numpy.where(img != 0)
     return img[min(p[0]) : max(p[0]) + 1, min(p[1]) : max(p[1]) + 1]
 
+def render_grid(alg, input_grid, i, j):
+    output = numpy.zeros((3, 3), dtype=int)
+    for delta_i in (-1, 0, 1):
+        for delta_j in (-1, 0, 1):
+            s = ''.join(str(v) for v in helpers.neighbors9_vals(input_grid, i + delta_i, j + delta_j))
+            n = int(s, 2)
+            output[1 + delta_i, 1 + delta_j] = int(alg[n], 2)
+    return output
+        
 def main() -> None:
     lines = helpers.read_input()
 
@@ -27,15 +36,9 @@ def main() -> None:
         new_img = numpy.zeros((padded_img.shape[0], padded_img.shape[1]), dtype=int)
         for i in range(padded_img.shape[0]):
             for j in range(padded_img.shape[1]):
-                sub_img = numpy.zeros((3, 3), dtype=int)
-                for delta_i in (-1, 0, 1):
-                    for delta_j in (-1, 0, 1):
-                        s = ''.join(str(v) for v in helpers.neighbors9_vals(padded_img, i + delta_i, j + delta_j))
-                        n = int(s, 2)
-                        sub_img[1 + delta_i, 1 + delta_j] = int(alg[n], 2)
-                s = ''.join(str(v) for v in helpers.neighbors9_vals(sub_img, 1, 1))
-                n = int(s, 2)
-                new_img[i, j] = int(alg[n], 2)
+                first_pass = render_grid(alg, padded_img, i, j)
+                second_pass = render_grid(alg, first_pass, 1, 1)
+                new_img[i, j] = second_pass[1, 1]
         img = new_img
         print(numpy.sum(img == 1))
     print(img)
