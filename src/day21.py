d0b3d3ccc9101280e48e9fa3490515703b60a20a
Chip Turner <cturner@pattern.net>
2021-12-20 22:10:21 -0800

    quick refactor

--- src/day21.py
+++ src/day21.py
@@ -4,37 +4,45 @@ import itertools
 import collections
 
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    pos: int
+    score: int
+
+    def advance(self, steps):
+        self.pos = 1 + (self.pos - 1 + steps) % 10
+        self.score += self.pos
+
+
 def main() -> None:
     lines = helpers.read_input()
     print(lines)
 
     rng = zip(itertools.count(1, 1), itertools.cycle(range(1, 101)))
-    p1_pos, p2_pos = 4, 10
-    p1_score, p2_score = 0, 0
-    combo = 0
-    winner_score = 0
+    p1 = Player(4, 0)
+    p2 = Player(8, 0)
     roll = []
-    
-    while True:
-        combo = (p1_score, p2_score)
-        roll = list(itertools.islice(rng, 3))
-        p1_pos += sum(v[1] for v in roll)
-        p1_pos = 1 + (p1_pos - 1) % 10
-        p1_score += p1_pos
 
-        if p1_score >= 1000:
+    while True:
+        combo = (p1.score, p2.score)
+        roll = list(itertools.islice(rng, 3))
+        p1.advance(sum(v[1] for v in roll))
+
+        if p1.score >= 1000:
             break
 
-        combo = (p1_score, p2_score)
+        combo = (p1.score, p2.score)
         roll = list(itertools.islice(rng, 3))
-        p2_pos += sum(v[1] for v in roll)
-        p2_pos = 1 + (p2_pos - 1) % 10
-        p2_score += p2_pos
+        p2.advance(sum(v[1] for v in roll))
 
-        if p2_score >= 1000:
+        if p2.score >= 1000:
             break
     print(roll)
     print(combo, combo[0] * combo[1])
     print(min(combo) * roll[-1][0])
+
 
 main()
